897fb8dde56c0446cfa6ce7a97dda7b9269c7b1a
Zylioth <amirsabrin8@gmail.com>
2024-10-01 09:38:50 +0800

    minor changes, includes on header

--- dashboard.php
+++ dashboard.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['user_id'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
+  <link rel="icon" href="Assets/IK_logo.png">
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>

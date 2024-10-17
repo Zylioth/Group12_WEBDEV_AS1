58e616bd79f34b19e0bf634d5f24f9d9d8819838
Zylioth <amirsabrin8@gmail.com>
2024-10-17 10:05:39 +0800

    portfolio user side & index css added responsive

--- portfolio.php
+++ portfolio.php
@@ -23,13 +23,10 @@ $result = $conn->query($query);
     <div class="portfolio-grid">
         <?php while ($row = $result->fetch_assoc()): ?>
             <div class="portfolio-item">
-                <img src="uploads/<?php echo $row['image']; ?>" alt="<?php echo $row['title']; ?>">
+                <img src="uploads/<?php echo $row['image_url']; ?>" alt="<?php echo $row['title']; ?>">
                 <h3><?php echo $row['title']; ?></h3>
                 <p><?php echo $row['description']; ?></p>
-                <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
-                    <a href="edit_portfolio.php?id=<?php echo $row['id']; ?>">Edit</a> |
-                    <a href="delete_portfolio.php?id=<?php echo $row['id']; ?>">Delete</a>
-                <?php endif; ?>
+
             </div>
         <?php endwhile; ?>
     </div>
